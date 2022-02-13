b5679156504de1d5526157442126965d9bb504b3
Andrew Marshall <andrewleonmarshall@gmail.com>
2022-02-13 12:20:43 -0500

    created queue for spawning building

--- Assets/Scripts/Units/UnitMovement.cs
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -54,10 +54,16 @@ public class UnitMovement : NetworkBehaviour
     [Command]
     public void CmdMove(Vector3 position)
     {
+        ServerMove(position);
+    }
+
+    [Server]
+    public void ServerMove(Vector3 position)
+    {
         targeter.ClearTarget();
 
         // Invalid Position
-        if(!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas)) { return; }
+        if (!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas)) { return; }
 
         agent.SetDestination(hit.position);
     }
